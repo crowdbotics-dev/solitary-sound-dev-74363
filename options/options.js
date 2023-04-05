fbf106ff58b2789bfdb0218c5ef7385978e5ea91
Crowdbotics <engineering@crowdbotics.com>
2023-04-05 01:14:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-sound-dev-74363",
+  url: "https://solitary-sound-dev-74363.botics.co",
+  api: "https://solitary-sound-dev-74363.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
